8ff5bd3bb2f93eb9fc7bb1451bbf870d3d163d70
Tom-LI-Core <tomli930728@gmail.com>
2025-08-06 22:34:01 +0100

    Create config.py

---
+++ fpl_ai/config.py
@@ -0,0 +1,12 @@
+README.md
+requirements.txt
+
+fpl_ai/__init__.py
+fpl_ai/config.py
+fpl_ai/data/fetch.py
+fpl_ai/data/process.py
+fpl_ai/models/lstm.py
+fpl_ai/optimiser/solver.py
+fpl_ai/scripts/update_data.py
+fpl_ai/scripts/week_run.py
+tests/__init__.py  (or empty folder)
